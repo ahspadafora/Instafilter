0233b81f429b2fe9b45daf8f6ec5919020484b74
Amber Spadafora <ahelenespadafora@gmail.com>
2020-12-17 16:46:29 -0500

    add save image to photo library functionality

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -18,6 +18,7 @@ struct ContentView: View {
     @State private var currentFilter: CIFilter = CIFilter.sepiaTone()
     
     @State private var showingFilterSheet = false
+    @State private var processedImage: UIImage?
     
     let context = CIContext()
     
@@ -61,7 +62,15 @@ struct ContentView: View {
                     }
                     Spacer()
                     Button("Save") {
-                        // save the picture
+                        guard let processedImage = self.processedImage else { return }
+                        let imageSaver = ImageSaver()
+                        imageSaver.successHandler = {
+                            print("Success!")
+                        }
+                        imageSaver.errorHandler = {
+                            print("oops: \($0.localizedDescription)")
+                        }
+                        imageSaver.writeToPhotoAlbum(image: processedImage)
                     }
                 }
             }
@@ -112,6 +121,7 @@ struct ContentView: View {
         if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
             let uiImage = UIImage(cgImage: cgimg)
             image = Image(uiImage: uiImage)
+            processedImage = uiImage
         }
     }
 }
