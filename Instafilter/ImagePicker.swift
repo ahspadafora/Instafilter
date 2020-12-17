db32dc5dba246819a79deb1cb2445974fcae60a4
Amber Spadafora <ahelenespadafora@gmail.com>
2020-12-17 16:01:10 -0500

    import an image into swiftUI using uiimagepickercontroller

---
+++ Instafilter/ImagePicker.swift
@@ -0,0 +1,49 @@
+//
+//  ImagePicker.swift
+//  Instafilter
+//
+//  Created by Amber Spadafora on 12/17/20.
+//  Copyright © 2020 Amber Spadafora. All rights reserved.
+//
+
+import SwiftUI
+
+struct ImagePicker: UIViewControllerRepresentable {
+    @Environment(\.presentationMode) var presentationMode
+    @Binding var image: UIImage?
+    
+    class Coordinater: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+        
+        let parent: ImagePicker
+        
+        init(_ parent: ImagePicker) {
+            self.parent = parent
+        }
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            if let uiImage = info[.originalImage] as? UIImage {
+                parent.image = uiImage
+            }
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+        
+    }
+    func makeCoordinator() -> Coordinater {
+        Coordinater(self)
+    }
+    
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.delegate = context.coordinator
+        return picker
+    }
+    
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {
+        
+    }
+    
+    
+    typealias UIViewControllerType = UIImagePickerController
+    
+    
+}
+
